a5ff459dbb316b6178a8d240792911e0a5e28e16
evan hadfield and kerri walker <evan@>
2019-03-28 11:03:04 -0700

    add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
+/*prompt for age
+
+/*branch
+
 $(document).ready() {
 
-};
+};*/
+
+$(document).ready(function(){
+  $(".form-group").submit(function(event){
+    console.log("clicked submit");
+  var num = parseInt($("#personAge.form-control").val());
+console.log(num);
+  if (num >= 18){
+
+    $("div .col-lg.ofAge").show();
+} else{
+    $("div .col-lg.minors").show();
+  }
+event.preventDefault();
+
+  });
+    });
